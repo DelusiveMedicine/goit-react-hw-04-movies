cc2b992e77a91af3f8f3e5fa1b1d987fd1aa89b0
Stacy <enjoyorhate@gmail.com>
2019-12-05 00:21:53 +0200

    Updates

---
+++ precache-manifest.5b9a47ea0de41547ad964e212b7579e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7684dce62c5382c2b6c9ed20d7c6b1",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "0075c2477045e92dcdb1",
+    "url": "/goit-react-hw-04-movies/static/js/2.bd474bbf.chunk.js"
+  },
+  {
+    "revision": "fa1605c0756c4a0b0664",
+    "url": "/goit-react-hw-04-movies/static/js/main.6cee4686.chunk.js"
+  },
+  {
+    "revision": "8c3d6f32c3559b72d5d7",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.50ef9e36.js"
+  }
+]);
